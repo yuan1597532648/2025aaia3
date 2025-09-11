44a9c6a8475dc589d8dd22444c63cb89e75bb4f7
yuan1597532648 <wayne20051105@gmail.com>
2025-09-11 17:41:26 +0800

    week01

---
+++ week01-2a.py
@@ -0,0 +1,8 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        H = len(haystack)
+        N = len(needle)
+        for i in range(H-N+1):
+            if haystack[ i : i+N ] == needle:
+                return i
+        return -1
