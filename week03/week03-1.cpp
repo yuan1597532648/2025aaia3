28c3a5dffddde00e9acab28128005083dec1dc80
yuan1597532648 <wayne20051105@gmail.com>
2025-09-25 17:25:44 +0800

    week03

---
+++ week03/week03-1.cpp
@@ -0,0 +1,16 @@
+#include <iostream> ///c++ ªº cin cout
+#include <vector> ///c++ ªº°}¦C vector
+using namespace std;
+int main()
+{
+    vector<int> a(2);
+
+    for(int i=0; i<a.size(); i++) cout << a[i] << ' ';
+    cout << endl;
+
+    a.push_back(99);
+    a.push_back(77);
+
+    for(int i=0; i<a.size(); i++) cout << a[i] << ' ';
+    cout << endl;
+}
