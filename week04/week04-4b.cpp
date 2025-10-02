48da8d23e11608b45aba7875144f4b345ab14de1
yuan1597532648 <wayne20051105@gmail.com>
2025-10-02 17:17:52 +0800

    week04

---
+++ week04/week04-4b.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	while( cin >> a >> b ){
+		int ans = 0;
+		int now=1;
+		while(n != 1){
+			if(n%2==1) n = 3*n+1;
+			else n = n / 2;
+			now++;
+		}
+		if(now>ans)ans = now;
+		printf("%d %d %d\n", a, b, ans);
+	}
+}
---
+++ week04/week04-4c.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	while( cin >> a >> b ){
+		int ans = 0;
+		for(int i=a; i<=b; i++){
+			int now=1;
+			int n = i;
+			while(n != 1){
+				if(n%2==1) n = 3*n+1;
+				else n = n / 2;
+				now++;
+			}
+			if(now>ans)ans = now;
+		}
+		printf("%d %d %d\n", a, b, ans);
+	}
+}
